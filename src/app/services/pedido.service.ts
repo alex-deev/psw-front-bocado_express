7383555f3fe242db02c8c2a4fd9b2a72ee4a9ca0
ax-star <ax.onestar@gmail.com>
2024-07-06 23:30:14 -0300

    Se mejora PedidoService
    Se implementa la funcionalidad de 'hacer pedidos' usando el backend.

--- src/app/services/pedido.service.ts
+++ src/app/services/pedido.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Producto } from '../models/producto';
 import { Pedido } from '../models/pedido';
+import { PedidoProducto } from '../models/pedidoProducto';
 
 @Injectable({
   providedIn: 'root'
@@ -10,27 +11,48 @@ import { Pedido } from '../models/pedido';
 export class PedidoService {
 
   private apiUrl = 'http://localhost:3000/api/';
-  private carrito: Producto[]=[];
 
-  constructor( private http: HttpClient ) { }
+  private carrito: Producto[] = [];
 
-  hacerPedido(pedido: Pedido): Observable<any> {
+  constructor( private http: HttpClient ) {}
+
+  hacerPedido(nuevoPedido: Pedido): Observable<any> {
     let httpOption = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })
     }
+    nuevoPedido.pedidoProductos = this.cargarProductosEnPedido();
+    // console.log(JSON.stringify(nuevoPedido));
+    return this.http.post(this.apiUrl + 'pedido', nuevoPedido, httpOption);
+  }
 
-    return this.http.post(this.apiUrl + 'pedido', pedido, httpOption);
+  cargarProductosEnPedido(): PedidoProducto[] {
+    return this.carrito.map(p => ({
+      producto: p.id,
+      cantidad: p.cantidad
+    }));
   }
 
   cargarCarrito(producto: Producto) {
-    this.carrito.push(producto);
-    //console.log(JSON.stringify(this.carrito));
+    // Busca en el carrito, por el id del producto que llega por parámetro
+    const indiceExiste = this.carrito.findIndex((p) => p.id == producto.id);
+
+    if(indiceExiste != -1) {
+      this.carrito[indiceExiste].cantidad ++ ;
+    } else {
+      producto.cantidad = 1;
+      this.carrito.push(producto);
+    }
+    // console.log(JSON.stringify(this.carrito));
   }
 
   obtenerCarrito(): Producto[] {
     return this.carrito;
+  }
+
+  vaciarCarrito() {
+    this.carrito = [];
   }
 
 }
